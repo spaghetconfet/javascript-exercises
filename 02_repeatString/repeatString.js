d8eee4d19d3fc1aca77a80e0e6900a44836bf9fd
spaghetconfet <67653932+spaghetconfet@users.noreply.github.com>
2025-01-09 13:28:13 -0500

    complete 01-03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if(num < 0) return 'ERROR';
+    let rstr = '';
+    for(let i = 0; i < num; ++i){
+        rstr += string;
+    }
 
+    return rstr;
 };
 
 // Do not edit below this line
